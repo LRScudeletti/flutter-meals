949279ed9a174cee36d12ea968313e62c27bebf4
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2021-12-12 19:21:26 -0300

    Adicionado estrutura de categorias

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_meals/components/category_item.dart';
+import 'package:projeto_meals/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Vamos cozinhar"),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCategories.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
