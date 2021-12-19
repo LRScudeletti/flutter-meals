7ed4e515338275d08a4546bfeb65eaf65eacfaa3
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2021-12-19 20:05:16 -0300

    Adicionado listview de receitas

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_meals/data/dummy_data.dart';
 import 'package:projeto_meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -7,14 +8,19 @@ class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por Categoria ${category.id}'),
-      ),
+      body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          }),
     );
   }
 }
