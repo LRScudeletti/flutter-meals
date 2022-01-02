7336847cb45544db41d8a31c330781ebabeccd27
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2022-01-02 09:50:26 -0300

    Adicionado rota para erros

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,22 @@ class MyApp extends StatelessWidget {
         AppRoutes.categoriesMeals: (ctx) => const CategoriesMealsScreen(),
         AppRoutes.mealDetail: (ctx) => const MealDetailScreen(),
       },
+      // onGenerateRoute: (settings) {
+      //   if (settings.name == '/alguma-coisa') {
+      //     return null;
+      //   } else if (settings.name == '/outra-coisa') {
+      //     return null;
+      //   } else {
+      //     return MaterialPageRoute(builder: (_) {
+      //       return const CategoriesScreen();
+      //     });
+      //   }
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (_) {
+          return const CategoriesScreen();
+        });
+      },
     );
   }
 }
