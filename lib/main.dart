c442240987c6bf7e8660a7f6c0fc99a22425680b
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2022-01-18 20:48:50 -0300

    Correção na main e na widget_test

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
         fontFamily: 'Raleway',
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
@@ -24,7 +22,7 @@ class MyApp extends StatelessWidget {
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
               ),
-            ),
+            ), colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink).copyWith(secondary: Colors.amber),
       ),
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(),
