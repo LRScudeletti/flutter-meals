67bd0db6f01640118173d83f417496b7feb94f64
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2022-02-14 22:04:27 -0300

    Correção de warnings na classe main

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,26 @@ import 'screens/settings_screen.dart';
 
 import 'utils/app_routes.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
       theme: ThemeData(
         fontFamily: 'Raleway',
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: TextStyle(
+              headline6: const TextStyle(
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
               ),
-            ), colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink).copyWith(secondary: Colors.amber),
+            ),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
       ),
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(),
